a246eee612f26fff9ab553ee56a9267d6d2f9541
VedantS28 <vedantshrivastava286@gmail.com>
2025-02-15 21:58:34 +0530

    Access to all ip added

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ connectDB();
 
 // Init Middleware
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type']
   }));app.use(express.json());
